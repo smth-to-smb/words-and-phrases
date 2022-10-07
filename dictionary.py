053d1c5dd4c675e495a6d5f93fa05b6845d0a2cb
Anton Samarin <Anton.Samarin@jetbrains.com>
2022-10-07 16:07:41 +0200

    Language selection, several more words

--- dictionary.py
+++ dictionary.py
@@ -7,7 +7,11 @@ while True:
     if word in ['q', 'Q']:
         break
     translation = input('Enter translation: ')
-    language = input('Enter the language: ')
+    language = input('Enter the language (1 for English, 2 for German): ')
+    if language == '1':
+        language = 'English'
+    elif language == '2':
+        language = 'German'
     insertion = 'INSERT INTO WORDS (WORD,TRANSLATION,LANGUAGE) VALUES (\'' + word + '\',' + '\'' + translation + '\',' + '\'' + language + '\')'
     conn.execute(insertion)
     conn.commit()
